00840ef429e591083ff86dc7f17a27d3e16689b3
Milan Fodor <40278965+MILANIUSZ@users.noreply.github.com>
2023-02-23 10:13:40 +0100

    attempt to align eeg with melspec

--- spectogram.py
+++ spectogram.py
@@ -58,18 +58,18 @@ def extractMelSpecs(audio, sr, windowLength=0.05, frameshift=0.01):
 
 # +
 #audio sampling rate +esampling the audio to a standard sampling rate of 22050 Hz and converting the audio data to mono etc
-audio_path = 'data/stimuli/6min.wav'
-y, sra = librosa.load(audio_path)
+#audio_path = 'data/stimuli/6min.wav'
+#y, sra = librosa.load(audio_path)
 
 
-
-# + active=""
-# librosa.get_samplerate(audio_path)
 # -
 
 filename = 'data/stimuli/6min.wav'
 audio, srb = sf.read(filename, dtype='float32')
        
+
+audio.shape
+
 
 # define the output directory path
 path_output = r'./features'
@@ -79,7 +79,7 @@ audio_sr = 48000
 original_audio_sr = 48000
         
 #Process Audio
-target_SR = 16000
+target_SR = 2048
 audio = scipy.signal.decimate(audio,int(audio_sr / target_SR))
 audio_sr = target_SR
 scaled = np.int16(audio/np.max(np.abs(audio)) * 32767)
@@ -87,29 +87,72 @@ os.makedirs(os.path.join(path_output), exist_ok=True)
 
 # -
 
-# write the rescaled audio to a WAV file in the output directory
-file_name = 'rescaled.wav'
-scipy.io.wavfile.write(os.path.join(path_output, file_name), audio_sr, scaled)
+#othermethods
+from scipy import signal
+audio, sr = librosa.load('data/stimuli/6min.wav', sr=48000)
+audio_resampled = signal.resample(audio, int(len(audio) / sr * 2048))
 
-#melspectrogram
+file_name = 'resampled.wav'
+scipy.io.wavfile.write(os.path.join(path_output, file_name), audio_sr, audio_resampled)
+
 winL = 0.05
 frameshift = 0.01
-melSpec = extractMelSpecs(scaled,audio_sr,windowLength=winL,frameshift=frameshift)
+melSpec = extractMelSpecs(audio_resampled, 2048, windowLength=winL, frameshift=frameshift)
+
+#import eeg (gamma from preprocessing)
+gamma = np.load("gamma.npy")
+
+gamma.shape
+
+
+if melSpec.shape[0] != gamma.shape[0]:
+    n_frames = min(melSpec.shape[0], gamma.shape[0])
+    melSpec = melSpec[:n_frames, :]
+    gamma = gamma[:n_frames, :]
+
+gamma.shape
+
+import matplotlib.pyplot as plt
+plt.plot(gamma[:,0])
+plt.show()
+
+plt.plot(melSpec[:,0])
+plt.show()
 
 # +
-#feat
-#import features (gamma
-# -
+import matplotlib.pyplot as plt
 
-#Align to EEG features
-       # melSpec = melSpec[modelOrder*stepSize:melSpec.shape[0]-modelOrder*stepSize,:]
-        #adjust length (differences might occur due to rounding in the number of windows)
-        #if melSpec.shape[0]!=feat.shape[0]:
-          #  tLen = np.min([melSpec.shape[0],feat.shape[0]])
-          #  melSpec = melSpec[:tLen,:]
-          #  feat = feat[:tLen,:]
-        
+fig, ax = plt.subplots(2, 1, sharex=True, figsize=(12, 6))
+
+# Plot EEG
+ax[0].plot(gamma)
+
+# Plot spectrogram
+im = ax[1].imshow(melSpec.T, aspect='auto', origin='lower')
+
+# Set axis labels and titles
+ax[0].set_ylabel('Amplitude')
+ax[1].set_ylabel('Frequency')
+ax[1].set_xlabel('Time')
+ax[0].set_title('EEG')
+ax[1].set_title('Spectrogram')
+
+# Add colorbar for spectrogram
+cbar = fig.colorbar(im, ax=ax[1])
+cbar.ax.set_ylabel('Magnitude')
+
+plt.show()
 
 
-        #Save everything
+# +
+start_time = 0 # set start time in seconds
+end_time = 2 # set end time in seconds
+
+# plot EEG and spectrogram for the specified time window
+fig, axs = plt.subplots(2, 1, figsize=(20,10))
+axs[0].plot(times[(times>=start_time) & (times<end_time)], gamma[(times>=start_time) & (times<end_time), :])
+axs[0].set_title('EEG')
+axs[1].imshow(melSpec[int(start_time*sr/stepSize):int(end_time*sr/stepSize), :].T, origin='lower', aspect='auto', cmap='jet', interpolation='none')
+axs[1].set_title('Mel Spectrogram')
+plt.show()
 
